83364caae0bc52260df57d1572a8b0354e6b2ea8
GrayWorldFinex <116639741+GrayWorldFinex@users.noreply.github.com>
2022-10-31 18:48:55 +0300

    Add files via upload

---
+++ GetIPAddress/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetIPAddress
+{
+    public class IPAddr
+    {
+        public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return null;
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            IPAddr xIP = new IPAddr();
+            File.WriteAllText(@"D:\Temp\IP.bin", xIP.GetLocalIPAddress());
+            Process.Start("notepad", @"D:\Temp\IP.bin");
+        }
+    }
+}
